bcc8df48445fcb3bdf8d8f9818f6f4aa0a06c861
siux <jpetar765@gmail.com>
2020-08-23 21:32:20 +0200

    added file download fixed attachment component display

--- src/app/components/attachment/attachment.component.ts
+++ src/app/components/attachment/attachment.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Attachment} from "../../model/Attachment";
-import { faCoffee,faDownload } from '@fortawesome/free-solid-svg-icons';
+import {faCoffee, faDownload, faFilePdf, faImage, faStarOfDavid} from '@fortawesome/free-solid-svg-icons';
+import {Post} from '../../model/Post';
 
 @Component({
   selector: 'app-attachment',
@@ -10,11 +11,28 @@ import { faCoffee,faDownload } from '@fortawesome/free-solid-svg-icons';
 export class AttachmentComponent implements OnInit {
 
   @Input() attachment: Attachment;
+  @Input() post: Post;
   public faDownload = faDownload;
+  faDocument = faFilePdf;
+  downloadUrl: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.resolveFileIconBytype();
+    this.downloadUrl = `http://localhost:8080/post/${this.post.id}/file/${this.attachment.originalName}`;
   }
 
+  resolveFileIconBytype(): any {
+    switch (this.attachment.originalName.substr(this.attachment.originalName.lastIndexOf('.') + 1)){
+      case 'png':
+        this.faDocument = faImage;
+        break;
+      case 'pdf':
+        this.faDocument =  faFilePdf;
+        break;
+      default:
+        this.faDocument = faStarOfDavid;
+    }
+  }
 }
