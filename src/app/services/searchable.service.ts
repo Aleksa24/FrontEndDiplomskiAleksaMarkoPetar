eb045068debffc9f7acabf2c35403d5f3aedf89b
Aleksa24 <aleksaivanovic24444@gmail.com>
2020-09-17 16:21:00 +0200

    komit za searchable.service.ts doradjeno

--- src/app/services/searchable.service.ts
+++ src/app/services/searchable.service.ts
@@ -7,17 +7,18 @@ import {Post} from "../model/Post";
 import {Channel} from "../model/Channel";
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {environment} from "../../environments/environment";
+import {AuthenticationService} from "../service/authentication/authentication.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchableService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient,
+              private authService: AuthenticationService) { }
 
   getChannelsAndPosts(filterValue: string): Observable<Searchable[]>{
-    // let params = new HttpParams();
-    // params.append('filterValue',filterValue);
-    return this.httpClient.get<Searchable[]>(`${environment.resourceServerUrl}/searchable/channels-and-posts?filterValue=${filterValue}`);
+    let userId = this.authService.getUserFromLocalCache().id;
+    return this.httpClient.get<Searchable[]>(`${environment.resourceServerUrl}/searchable/channels-and-posts?filterValue=${filterValue}&userId=${userId}`);
   }
 }
